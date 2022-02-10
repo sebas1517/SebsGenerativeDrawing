6331cc65fadfbfc69d8e4759180dbf239cce8ca1
ssebas1517 <ss61@uic.edu>
2022-02-10 15:32:13 -0600

    removed practice js and updated code/ read me

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SebsGenerativeDrawing
+- There will be a setup of lines that are displayed horizontally but are spaced out vertically. 
+- There is a component that will allow the lines to be adjusted simultaneously based off of the noise that is created. (slider in program does this)
+- Lines start from a dormant position and incrementally rise as the noise value rises.
--- sketch.js
+++ sketch.js
@@ -1,18 +1,53 @@
-/*function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-  }*/
+let yoff = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight-20);
   
-  let detailY;
-  // slide to see how detailY works
-  function setup() {
-    createCanvas(windowWidth-30, windowHeight-30, WEBGL);
-    detailY = createSlider(3, 16, 3);
-    detailY.position(10, height + 5);
-    detailY.style('width', '80px');
-  }
+  line1 = new Lines(windowHeight-20);
+  line2 = new Lines(windowHeight/2);
+  line3 = new Lines(windowHeight/3);
+  line4 = new Lines(windowHeight/4);
+  line5 = new Lines(windowHeight/5);
+  line6 = new Lines(windowHeight/6);
+  line7 = new Lines(windowHeight/7);
+  line8 = new Lines(windowHeight/8);
+
   
-  function draw() {
-    background(205, 105, 94);
-    rotateY(millis() / 1000);
-    sphere(40, 16, detailY.value());
+  slider = createSlider(0.0, 2,0, 0.0, 0.05);
+}
+
+function draw() {
+  background(255);
+
+  line1.change();
+  line2.change();
+  line3.change();
+  line4.change();
+  line5.change();
+  line7.change();
+  line8.change();
+
+}
+
+class Lines {
+  constructor(y) {
+    this.y = y;
   }
+
+  change() {
+    beginShape();
+    stroke(50);
+    strokeWeight(4);
+    noFill();
+    let xoff = 0;
+    for (let x = 0; x <= windowWidth; x += 1) {
+      let y = this.y - noise(xoff, yoff) * 120 * slider.value();
+      vertex(x,y);
+      xoff += 0.01;
+    }
+    yoff += 0.01;
+    vertex(windowWidth, windowHeight);
+    vertex(0, windowHeight);
+    endShape(CLOSE);
+  }
+}
